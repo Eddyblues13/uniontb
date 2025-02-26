4b82661280efc2c07f9438ed33db46dabe88bc83
eddyblues13 <eddyblues13@gmail.com>
2025-02-25 23:17:55 -0800

    changes

--- app/Http/Controllers/User/TransferController.php
+++ app/Http/Controllers/User/TransferController.php
@@ -37,27 +37,28 @@ class TransferController extends Controller
 
         $data['currentMonth'] = Carbon::now()->format('M Y'); // Example: "Feb 2025"
 
-        $data['totalSavingsCredit'] = SavingsBalance::whereMonth('created_at', Carbon::now()->month)
+        $data['totalSavingsCredit'] = SavingsBalance::where('user_id', $user->id)
+            ->whereMonth('created_at', Carbon::now()->month)
             ->whereYear('created_at', Carbon::now()->year)
             ->where('type', 'credit')
             ->sum('amount');
 
-        $data['totalSavingsDebit'] = SavingsBalance::whereMonth('created_at', Carbon::now()->month)
+        $data['totalSavingsDebit'] = SavingsBalance::where('user_id', $user->id)
+            ->whereMonth('created_at', Carbon::now()->month)
             ->whereYear('created_at', Carbon::now()->year)
             ->where('type', 'debit')
             ->sum('amount');
 
-
-
-
-        $data['totalCheckingCredit'] = CheckingBalance::whereMonth('created_at', Carbon::now()->month)
+        $data['totalCheckingCredit'] = CheckingBalance::where('user_id', $user->id)
+            ->whereMonth('created_at', Carbon::now()->month)
             ->whereYear('created_at', Carbon::now()->year)
             ->where('type', 'credit')
             ->sum('amount');
 
 
 
-        $data['totalCheckingDebit'] = CheckingBalance::whereMonth('created_at', Carbon::now()->month)
+        $data['totalCheckingDebit'] = CheckingBalance::where('user_id', $user->id)
+            ->whereMonth('created_at', Carbon::now()->month)
             ->whereYear('created_at', Carbon::now()->year)
             ->where('type', 'debit')
             ->sum('amount');
